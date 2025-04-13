b62010b4da8698a04b635859201d138b8ca160b8
Kazuya Oki <kazuyaoki@aldagram.com>
2025-04-13 17:12:14 +0900

    apply biome format

--- frontend/src/ui/LoadingSkeleton.tsx
+++ frontend/src/ui/LoadingSkeleton.tsx
@@ -1,19 +1,19 @@
 import React from "react";
 
 export const LoadingSkeleton = () => {
-	return (
-		<div
-			style={{
-				marginBottom: "12px",
-				padding: "10px 15px 10px 0",
-				borderRadius: "18px",
-				maxWidth: "70%",
-				position: "relative",
-				animation: "pulse 1.5s infinite ease-in-out",
-			}}
-		>
-			<p>通信中...</p>
-			<style jsx>{`
+  return (
+    <div
+      style={{
+        marginBottom: "12px",
+        padding: "10px 15px 10px 0",
+        borderRadius: "18px",
+        maxWidth: "70%",
+        position: "relative",
+        animation: "pulse 1.5s infinite ease-in-out",
+      }}
+    >
+      <p>通信中...</p>
+      <style jsx>{`
         @keyframes pulse {
           0% {
             opacity: 0.6;
@@ -26,6 +26,6 @@ export const LoadingSkeleton = () => {
           }
         }
       `}</style>
-		</div>
-	);
+    </div>
+  );
 };
--- frontend/src/ui/UserSentMessage.tsx
+++ frontend/src/ui/UserSentMessage.tsx
@@ -1,31 +1,31 @@
 export type UserSentMessageType = {
-	id: string;
-	content: string;
+  id: string;
+  content: string;
 };
 
 export const UserSentMessage = ({ content }: UserSentMessageType) => {
-	return (
-		<div
-			style={{
-				display: "flex",
-				justifyContent: "flex-end",
-				marginBottom: "12px",
-			}}
-		>
-			<div
-				style={{
-					backgroundColor: "#00A26A",
-					color: "white",
-					padding: "10px 15px",
-					borderRadius: "18px",
-					maxWidth: "70%",
-					position: "relative",
-					boxShadow: "0 1px 2px rgba(0, 0, 0, 0.1)",
-					whiteSpace: "pre-wrap",
-				}}
-			>
-				{content}
-			</div>
-		</div>
-	);
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "flex-end",
+        marginBottom: "12px",
+      }}
+    >
+      <div
+        style={{
+          backgroundColor: "#00A26A",
+          color: "white",
+          padding: "10px 15px",
+          borderRadius: "18px",
+          maxWidth: "70%",
+          position: "relative",
+          boxShadow: "0 1px 2px rgba(0, 0, 0, 0.1)",
+          whiteSpace: "pre-wrap",
+        }}
+      >
+        {content}
+      </div>
+    </div>
+  );
 };
